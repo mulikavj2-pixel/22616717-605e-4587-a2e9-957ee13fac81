bc490b7aee13635c06d5f989d8133c2e1aef6014
mulikavj2-pixel <mulikavj2@gmail.com>
2025-12-16 16:09:21 +0200

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { ThemeProvider } from "@/providers/themeProvider/ThemeProvider";
 import NavbarLayoutFloatingOverlay from '@/components/navbar/NavbarLayoutFloatingOverlay/NavbarLayoutFloatingOverlay';
 import HeroOverlayBottomSplit from '@/components/sections/hero/HeroOverlayBottomSplit';
